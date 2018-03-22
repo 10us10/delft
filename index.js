4bfc8340f7d1275ff020311fac62518052009ebc
Zendovo <33803331+Zendovo@users.noreply.github.com>
2018-03-22 15:32:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity(`Hey, Wassup!`);
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
+  }
+});
+
+bot.login(process.env.token);
