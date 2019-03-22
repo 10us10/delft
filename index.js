0488690eb66451755d2b79589fde9c12ea620c32
10us10 <48697735+10us10@users.noreply.github.com>
2019-03-22 13:23:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '-';
+  let prefix = '=';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
